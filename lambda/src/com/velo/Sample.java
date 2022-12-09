8d81b3c7e3ee5001a8cc0beda11a912b1d050a2a
Lenovo <Lenovo@LAPTOP-67V8L41N>
2022-12-09 12:07:57 +0530

    first commit

---
+++ lambda/src/com/velo/Sample.java
@@ -0,0 +1,6 @@
+package com.velo;
+
+public interface Sample {
+
+	public int read(int a);
+}
---
+++ lambda/src/com/velo/SampleIMPL.java
@@ -0,0 +1,11 @@
+package com.velo;
+
+public class SampleIMPL {
+
+	public static void main(String[] args) {
+		
+		Sample sample = (c)->(c);
+		System.out.println(sample.read(10));
+	}
+	
+}
